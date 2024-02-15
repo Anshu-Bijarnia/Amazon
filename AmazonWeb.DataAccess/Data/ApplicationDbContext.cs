9c0a649c35a1cadb9594e6abb5d3aa32bf1f8a7b
Anshu-Bijarnia <ansh.dtu@gmail.com>
2024-02-15 17:48:04 +0530

    Added scaffolding Identity

--- AmazonWeb.DataAccess/Data/ApplicationDbContext.cs
+++ AmazonWeb.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Amazon.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Amazon.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -13,6 +15,8 @@ namespace Amazon.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "Science Fiction", DisplayOrder = 2 },
